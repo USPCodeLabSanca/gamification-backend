a05484e19673f9192ca13729da1e79cce75d4560
rafaelmonteiro95 <rafaelmonteiro95@gmail.com>
2019-02-07 10:27:00 -0200

    WIP

---
+++ src/middlewares/auth.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken'
+import authConfig from '../auth.json'
+
+export default (req, res, next) => {
+    let authHeader = req.headers.authorization;
+
+    // checking if token exists
+    if (!authHeader)
+        return res.status(401).send({error: 'No token provided'});
+
+    // checking if token is well formated
+    // jwt format: bearer <hash>
+    let parts = authHeader.split(' ');
+    if(parts.length === 2)
+        return res.status(401).send({error: 'Token error'});
+
+    // splits token in two
+    let [ scheme, token ] = parts;
+
+    // checks if token is well formatted
+    if (!/^Bearer$/i.test(scheme))
+        return res.status(401).send({error: 'Bad token format'});
+
+    // checks if token is valid
+    jwt.verify(token, authConfig.secret, (err, decoded) => {
+        // if an error occurs
+        if(err) return res.status(401).send({ error: 'Invalid Token' });
+        // else gives the decoded userID
+        req.userId = decoded.id
+        return next();
+    });
+};
